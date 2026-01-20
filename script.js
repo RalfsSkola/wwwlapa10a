3d4b8057b29788a45af0a8af33c365db548a64b3
RalfsSkola <ralfs.buris@edu.kekava.lv>
2026-01-20 10:20:46 +0200

    ievads4

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function poga(){
     window.location.href="distancu.html"
 }
+function poga1(){
+    window.location.href="kalnu.html"
+}
+function poga2(){
+    window.location.href="snovosana.html"
+}
